32a3909d15f4b81a22a4ddb358b4715518df8539
Chirag Goyal <chirag.goyal@freecharge.com>
2018-06-13 10:23:46 +0530

    Trial run

---
+++ src/main/java/com/chirag/FromScratchSecondApplication.java
@@ -0,0 +1,12 @@
+package com.chirag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromScratchSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromScratchSecondApplication.class, args);
+	}
+}
